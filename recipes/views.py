eb71623e2dd30d7e0a4ac3bd289f6499e760e7ee
Luan Brito <luan.filipinb@gmail.com>
2023-03-20 15:34:30 -0300

    incluido a opção de receber receitas em html

--- recipes/views.py
+++ recipes/views.py
@@ -26,6 +26,13 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+
+    recipe = get_list_or_404(Recipe.objects.filter(
+        pk=id,
+        is_published=True
+        ).order_by('-id').first()
+    )
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True,
