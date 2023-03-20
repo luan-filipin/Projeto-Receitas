30cdc372273e7d36a67f62b72f732f109cd19008
Luan Brito <luan.filipinb@gmail.com>
2023-03-20 14:21:48 -0300

    Tratando receitas inexistentes.

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
@@ -14,12 +14,14 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
         is_published=True
-    ).order_by('-id')
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category '
     })
 
 
